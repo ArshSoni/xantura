04543ade0b6080b4ee35159526cec35c7c10d2c2
Arshdeep Soni <arshdsoni@gmail.com>
2026-06-03 21:51:17 +0100

    feat: ui cleanup

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -16,28 +16,27 @@ export const ProductDetailPage = () => {
 		return <Spinner />
 	}
 
-	if ( !foundProduct ) {
-		return (
-			<div className="p-6">Product not found</div>
-		)
-	}
 	return (
 		<section className="min-h-screen flex flex-col items-center justify-center">
-			<div className="w-full max-w-[700px] p-8 border-2 border-gray-100 rounded-2xl shadow">
+			<div className="w-full max-w-175 p-8 border-2 border-gray-100 rounded-2xl shadow">
 				<Link to="/" className="hover:underline cursor text-left w-full" onClick={() => setSelectedProductId(null)}>Back to products</Link>
 
-				<div className="flex flex-col md:flex-row gap-8 items-center md:items-start mt-5">
-					<img
-						src={foundProduct.image}
-						alt={foundProduct.title}
-						className="w-64 h-64 object-contain"
-					/>
-					<div className="text-left">
-						<h1 className="text-3xl font-bold">{foundProduct.title}</h1>
-						<p className="mt-4 text">{foundProduct.description}</p>
-						<p className="mt-4 text-xl">£{foundProduct.price}</p>
+				{!foundProduct && <div className='text-center'>Product not found</div>}
+
+				{foundProduct && (
+					<div className="flex flex-col md:flex-row gap-8 items-center md:items-start mt-5">
+						<img
+							src={foundProduct.image}
+							alt={foundProduct.title}
+							className="w-64 h-64 object-contain"
+						/>
+						<div className="text-left">
+							<h1 className="text-3xl font-bold">{foundProduct.title}</h1>
+							<p className="mt-4 text">{foundProduct.description}</p>
+							<p className="mt-4 text-xl">£{foundProduct.price}</p>
+						</div>
 					</div>
-				</div>
+				)}
 			</div>
 
 		</section>
